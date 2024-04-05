bd80ca311e98e7d6e3a707c4b3e9e6ae57ae4285
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-04-05 14:01:52 -0300

    asd

--- src/api/helper/genVc.js
+++ src/api/helper/genVc.js
@@ -2,8 +2,8 @@ module.exports = function generateVC(data) {
     const result =
         'BEGIN:VCARD\n' +
         'VERSION:3.0\n' +
-        `FN:${data.fullName}\n` +
-        //ORG:${data.organization};\n` +
+        //`FN:${data.fullName}\n` +
+        `ORG:${data.fullName};\n` +
         `TEL;type=CELL;type=VOICE;waid=${data.phoneNumber}:+${data.phoneNumber}\n` +
         'END:VCARD'
 
