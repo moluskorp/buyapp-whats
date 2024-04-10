1db1a7e40fd1426f62f070ca94ac78e7894cf729
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-04-10 20:17:20 -0300

    a

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -356,12 +356,13 @@ class WhatsAppInstance {
                                 if(match) {
                                     const number = match[1]
                                     const contact = await getContato(number, this.empresaId)
+                                    console.log({number, idEmpresa: this.empresaId})
                                     console.log({contact})
                                     if(!contact) {
                                         const newContact = await sendDataToSupabase('contatos', {
                                             nome: displayName,
                                             numero: number,
-
+                                            ref_empresa: this.empresaId
                                         })
                                         contactId = newContact.id
                                     } else {
